1de09b7b412fbafb50b9dc36846aaac0fa8ef4d3
Bass-020305bark <baskarkrab02@gmail.com>
2026-01-28 16:54:40 +0530

    side nav par added

--- src/Componends/SideNavPar.jsx
+++ src/Componends/SideNavPar.jsx
@@ -1,6 +1,14 @@
 
 export default function SideNavPar(){
     
-    
+    return(
+        <>
+            <div className="navPArent">
+                <div className="my-folder">
+                    <h1>My folder</h1>
+                </div>
+            </div>
+        </>
+    )
 
 }

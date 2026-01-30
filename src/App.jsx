67318c571fb65466452f703e0d0d0ac54a6b4b86
krishnakumarprabha-ms <krishnakumarprabha2008@gmail.com>
2026-01-30 17:12:23 +0530

    Pulling updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,22 @@ function App() {
   return (
     <>
       {/* <Home></Home> */}
+<<<<<<< HEAD
        <Routes>
+=======
+       {/* <Routes>
+>>>>>>> fe77f4b (Pulling updated UI)
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/" element={<SignIn/>} />
         {/* <Route path="/edit" element={<EditPage />} /> */}
+<<<<<<< HEAD
         <Route path="/home" element={<Home />} />
       </Routes>
+=======
+        {/* <Route path="/home" element={<Home />} />
+      </Routes> */}
+      <ReplacePopup cancel={()=>{}}>My Folder</ReplacePopup>
+>>>>>>> fe77f4b (Pulling updated UI)
     </>
   )
 }

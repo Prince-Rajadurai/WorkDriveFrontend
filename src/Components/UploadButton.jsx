76c7399713969c6972f334feeff127d039d38333
Bass-020305bark <baskarkrab02@gmail.com>
2026-02-02 14:14:59 +0530

    add upload button code

--- src/Components/UploadButton.jsx
+++ src/Components/UploadButton.jsx
@@ -1,10 +1,10 @@
-import '../Style/UploadButton.css'
+import '../Style/UploadButton.css';
 
-export default function UploadButton(){
+export default function UploadButton({sendValue}){
     return(
         <>
             <label htmlFor="upload_btn" className='btn-upload'>Upload File</label>
-            <input type="file" id="upload_btn" hidden/>
+            <input type="file" id="upload_btn" onChange={(e)=>{sendValue(e.target.value)}} hidden/>
         </>
     );
 }

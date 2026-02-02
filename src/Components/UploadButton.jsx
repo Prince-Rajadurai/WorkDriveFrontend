a8b66e2a3a5699d33bf1863ab09644b9f7580f78
Bass-020305bark <baskarkrab02@gmail.com>
2026-02-02 14:11:45 +0530

    pull the code

--- src/Components/UploadButton.jsx
+++ src/Components/UploadButton.jsx
@@ -1,4 +1,4 @@
-import '../Style/UploadButton.css';
+import '../Style/UploadButton.css'
 
 export default function UploadButton({sendValue}){
     return(
@@ -6,5 +6,10 @@ export default function UploadButton({sendValue}){
             <label htmlFor="upload_btn" className='btn-upload'>Upload File</label>
             <input type="file" id="upload_btn" onChange={(e)=>{sendValue(e.target.value)}} hidden/>
         </>
+=======
+export default function UploadButton({sendValue}){
+    return(
+        <input type="file" value ="Upload File" on/>
+>>>>>>> 0630b78 (pull the code)
     );
 }
